156fc7165c1f65c8b92db443933dfbf97a1b8361
najmaraissa <najmaraissazhr@gmail.com>
2023-06-12 14:47:59 +0700

    Membuat class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -10,5 +10,13 @@ package Hitung;
  * @author Nazma
  */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika najma = new Matematika(3,1);
+        
+        System.out.println("Hasil Penjumlahan:" + najma.setPenjumlahan());
+        System.out.println("Hasil Pengurangan:" + najma.setPengurangan());
+        System.out.println("Hasil Perkalian:" + najma.setPerkalian());
+        System.out.println("Hasil Pembagian:" + najma.setPembagian());
+    }
+         
 }
