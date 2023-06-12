54c3142394739215330445736915c61751ee8099
najmaraissa <najmaraissazhr@gmail.com>
2023-06-12 14:28:06 +0700

    Membuat constructor dan setPenjumlahan

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -10,5 +10,14 @@ package Hitung;
  * @author Nazma
  */
 public class Matematika {
+    private double bil1, bil2;
     
+    public Matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
 }
